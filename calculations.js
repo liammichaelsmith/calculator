6736fd3fb6d093cd6cf4ac62b127149895d8bd70
Riddos <ixausliam@gmail.com>
2018-05-10 12:02:35 +1000

    Add files via upload

---
+++ calculations.js
@@ -0,0 +1,16 @@
+$(document).on("change", ".qty1", function() {
+    var sum = 0;
+    $(".qty1").each(function(){
+        sum += +$(this).val();
+    });
+    $(".total").val(sum);
+});
+
+$(document).on("change", ".qty2", function() {
+    var sum = 0;
+    $(".qty2").each(function(){
+        sum += +$(this).val();
+    });
+    $(".total2").val(sum);
+});
+
